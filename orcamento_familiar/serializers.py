4f9e1c332e546e7f92060f225f83ef27ac126a00
Rafael Araujo <rafael.araujo.ds@outlook.com>
2022-08-05 09:50:14 -0300

    Seriealizer do cadastro de receita.

---
+++ orcamento_familiar/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from orcamento_familiar.models import Transacao
+
+class ReceitaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transacao
+        fields = ['id', 'descricao', 'valor', 'data']
+
+    def validate(self, data):
+        """
+        Verifica se existe receita com a mesma descrição para este mês.
+        """
+        queryset = Transacao.objects.filter(descricao=data['descricao'], data__month=data['data'].month)
+        if queryset:
+            raise serializers.ValidationError("Receita duplicada - esta receita já existe neste mês.")
+        return data
+
+    def create(self, validated_data):
+        validated_data['tipo'] = 'R'
+        return super(ReceitaSerializer, self).create(validated_data)
